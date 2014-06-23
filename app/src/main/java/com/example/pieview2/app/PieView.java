a1c0133d157199166a8cedd853c4f4e86117afe5
Benjamin Carlson <carlson.j.ben@gmail.com>
2014-06-22 22:20:46 -0400

    Add some documentation

--- app/src/main/java/com/example/pieview2/app/PieView.java
+++ app/src/main/java/com/example/pieview2/app/PieView.java
@@ -27,8 +27,7 @@ public class PieView extends ViewGroup {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         final int count = getChildCount();
 
-        int curWidth, curHeight, curLeft, curRight;
-
+        int curWidth, curHeight;
         final int childLeft = this.getPaddingLeft();
         final int childRight = this.getMeasuredWidth() - this.getPaddingRight();
         final int childTop = this.getPaddingTop();
@@ -36,26 +35,33 @@ public class PieView extends ViewGroup {
         final int childWidth = childRight - childLeft;
         final int childHeight = childBottom - childTop;
 
+        // Number of degrees between each child view
         final float degreeSpacing = 360.0f / count;
 
-
+        // As the PieView spirals out from the center, find the center of our view
         final int viewCenterX = this.getMeasuredWidth() / 2;
         final int viewCenterY = this.getMeasuredHeight() / 2;
 
         for (int i = 0; i < count; i++) {
             View child = getChildAt(i);
 
+            // Get the width and height requested by the child
             child.measure(MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.AT_MOST),
                           MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.AT_MOST));
 
+            // Use this in the child.layout() call to set the width (right - left)
+            // and height (bottom-top) of the child
             curWidth = child.getMeasuredWidth();
             curHeight = child.getMeasuredHeight();
 
+            // Determines the cx and cy positions of the child view
+            // based on angle and radius.  Angle is determined by child number
+            // and angle spacing (360 / number of children)
             final double rad = Math.toRadians(degreeSpacing * i);
-
             int viewX = viewCenterX + mRadius * (int)Math.cos(rad);
             int viewY = viewCenterY + mRadius * (int)Math.sin(rad);
 
+            // Call to layout child view
             child.layout(viewX, viewY, viewX + curWidth, viewY + curHeight);
         }
     }
